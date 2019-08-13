6727446584b45ca4f224e089e526cfd06bfaa8af
Ricardo Cabrera <ricky_92rc@hotmail.com>
2019-08-12 23:46:51 -0700

    linked cocktail search text form to cocktail api key

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,25 +1,33 @@
-
 // Cocktail API from cocktail DB
 
-var search_text = "margarita"
-var url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${search_text}`
+$('#cocktailBtn').on('click', function(){
+    var search_text = $('#searchCocktail').val();
+
+    var u = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${search_text}`
+
+    console.log(u)
 
     $.ajax({
-  url: url,
-  method: "GET"
-}).then(function (response) {
-  console.log(response)
-})
+      url: u,
+      method: "GET"
+    }).then(function(response) {
+
+      var myJSON = JSON.stringify(response.drinks);
+      $('#cocktail').text(myJSON);
+    });
+
+    event.preventDefault();
+  });
 
 
-// Beer API from brewery DB
 
-var url = "https://cors-anywhere.herokuapp.com/https://sandbox-api.brewerydb.com/v2/"
-    var key = "key=a8f6c358f10c7a5db840633a6ea304f9"
-    var endpoint = "beer/random/?"
-$.ajax({
-url: url + endpoint + key,
-method: "GET"
-}).then( function(response){
-    console.log(response)
-})
+// // Beer API from brewery DB
+
+// var url = "https://cors-anywhere.herokuapp.com/https://sandbox-api.brewerydb.com/v2/"
+//     var key = "key=a8f6c358f10c7a5db840633a6ea304f9"
+//     var endpoint = "beers/?"
+// $.ajax({
+// url: url + endpoint + key,
+// method: "GET"
+// }).then( function(response){
+//     console.log(response.data)})
